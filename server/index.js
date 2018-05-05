08af0c976dd8e10b995a77f28911633bd4059634
joaquin <joakin@ciencias.unam.mx>
2018-05-05 21:37:08 +0000

    Feaching sensor readings using XHR

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const  app =  express();
 
-const getCacheSensorReadings = require('./get-cached-sensor-readings');
+const getCachedSensorReadings = require('./get-cached-sensor-readings');
 const path = require('path');
 
 app.use('/public', express.static(path.join(__dirname,'public')))
 
 app.get('/temperature', function(req, res){
-	res.send('<strong>' +
-		getCacheSensorReadings.getTemperature().toFixed(1)
+	res.send('<strong>' +getCachedSensorReadings.getTemperature().toFixed(1)
 		 + '</strong>' + 'C')
 })
 
 app.get('/humidity', function(req, res){
-	res.send(getCacheSensorReadings.getHumidity().toFixed(1) + '%')
+	res.send('<strong>' + getCachedSensorReadings.getHumidity().toFixed(1)
+		 + '</strong>' + '%')
 })
 
 
---
+++ server/public/script.js
@@ -0,0 +1,30 @@
+const fetchTemperature = () => {
+	fetch('/temperature')
+	   .then(results => {
+		return results.text()
+	   }
+	)
+	.then(text => {
+           const temperatureDisplay =
+           document.getElementById('temperature-display')
+           temperatureDisplay.innerHTML = text
+	})
+}
+
+const fetchHumidity = () => {
+        fetch('/humidity')
+           .then(results => {
+                return results.text()
+           }
+        )
+        .then(text => {
+           const temperatureDisplay =
+           document.getElementById('humidity-display')
+           temperatureDisplay.innerHTML = text
+        })
+}
+
+setInterval(() => {
+	fetchTemperature()
+	fetchHumidity()
+	}, 2000)
