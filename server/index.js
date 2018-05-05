2ed7688ce3b490a55d870b147be19e43bb2b6baa
joaquin <joakin@ciencias.unam.mx>
2018-05-05 15:24:33 +0000

    beginning with UX

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const  app =  express();
 
 const getCacheSensorReadings = require('./get-cached-sensor-readings');
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname,'public')))
 
 app.get('/temperature', function(req, res){
-	res.send(getCacheSensorReadings.getTemperature().toFixed(1) + 'C')
+	res.send('<strong>' +
+		getCacheSensorReadings.getTemperature().toFixed(1)
+		 + '</strong>' + 'C')
 })
 
 app.get('/humidity', function(req, res){
 	res.send(getCacheSensorReadings.getHumidity().toFixed(1) + '%')
-});
+})
+
 
 app.listen(3000, function(){
 	console.log('Server listening on port 3000');
