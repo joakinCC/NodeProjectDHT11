2aa9428275701cfef333365470fa739bfd092198
joakinRaspi <joakin@ciencias.unam.mx>
2018-06-10 21:47:20 +0000

    resulto problema de base y promedios

--- server/database-operations.js
+++ server/database-operations.js
@@ -3,21 +3,21 @@ const path = require('path')
 const db = new sqlite3.Database(path.resolve('./.sqlite.db'))
 
 const insertReadings = (type, reading) => {
-	db.run(`INSERT INTO ${type} VALUES (datetime('now'), ${reading});`)
+	db.run(`INSERT INTO '${type}' VALUES (datetime('now'), ${reading});`)
 }
 
 const fetchLatestReadings = (type, limit, callback) => {
-	db.all(`SELECT * FROM temperature ORDER BY createdAt
+	db.all(`SELECT * FROM '${type}' ORDER BY createdAt
 	DESC LIMIT $(limit)`, callback)
 }
 
 const fetchReadingsBetweenTime = (type, start, end, callback) => {
-	db.all(`SELECT * FROM temperature WHERE createdAt > ?
+	db.all(`SELECT * FROM '${type}' WHERE createdAt > ?
 	AND createdAt < ? ORDER BY createdAt ASC;`,[start, end], callback)
 }
 
 const getAverageOfReadingsBetweenTime = (type, start, end, callback) => {
-	db.get(`SELECT avg(value) FROM temperature WHERE createdAt > ? AND createdAt < ?;`, [start, end], callback)
+	db.get(`SELECT avg(value) FROM '${type}' WHERE createdAt > ? AND createdAt < ?;`, [start, end], callback)
 }
 
 module.exports ={
